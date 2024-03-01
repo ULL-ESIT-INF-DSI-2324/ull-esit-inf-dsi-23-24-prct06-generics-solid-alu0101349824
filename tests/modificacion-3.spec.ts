37d656d71bb7ee5e1157389b840044955de80635
Laura Álvarez Zamora <alu0101349824@ull.edu.es>
2024-03-01 17:00:15 +0000

    Realizadas las pruebas para la modificación arreglada.

--- tests/modificacion-3.spec.ts
+++ tests/modificacion-3.spec.ts
@@ -50,23 +50,30 @@ describe("Modificación 3", () => {
       expect(a.multiply(b).toString()).to.equal("2/9");
     });
 
-    it("Debe manejar la división por un racional que resulta en denominador cero", () => {
+    it("Debe dividir dos números racionales correctamente", () => {
       const a = new Rational(1, 3);
-      const c = new Rational(0, 1);
-      expect(() => a.divide(c).toString()).to.throw(
-        "El denominador no puede ser cero.",
-        );
-      });
-      
-      it("Debe simplificar un racional", () => {
-        const rational = new Rational(3, 6);
-        expect(rational.numerator).to.be.eql("1");
-        expect(rational.denominator).to.be.eql("3");
-      });
+      const c = new Rational(7, 8);
+      expect(a.divide(c).toString()).to.equal("8/21");
+    });
+
+    it("Debe simplificar un racional", () => {
+      const rational = new Rational(3, 6);
+      expect(rational.numerator).to.equal(1);
+      expect(rational.denominator).to.equal(2);
+    });
 
     it("Debe mandar un error cuando el denominador es cero", () => {
       expect(() => new Rational(1, 0)).to.throw(
         "El denominador no puede ser cero.",
+      );
+    });
+
+    it("Debe lanzar un error al intentar dividir por un número racional con numerador 0", () => {
+      const a = new Rational(1, 2);
+      const b = new Rational(0, 1); // Racional con numerador 0 que no debería ser usado para división
+      expect(() => a.divide(b)).to.throw(
+        Error,
+        "No se puede dividir por un número racional con numerador 0.",
       );
     });
   });
